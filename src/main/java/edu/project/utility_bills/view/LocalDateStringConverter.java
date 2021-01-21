aad5740ccde1c2914a68b5150e623d5b9c38894d
assaulov <assaulov.i@gmail.com>
2021-01-21 22:13:38 +0200

    save and delete utilities

---
+++ src/main/java/edu/project/utility_bills/view/LocalDateStringConverter.java
@@ -0,0 +1,15 @@
+package edu.project.utility_bills.view;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateStringConverter extends StdConverter<LocalDate, String> {
+
+    static final String DATE_FORMAT = "dd.MM.yyyy";
+    @Override
+    public String convert(LocalDate value) {
+        return value == null ? "" :value.format(DateTimeFormatter.ofPattern(DATE_FORMAT));
+    }
+}
---
+++ src/main/java/edu/project/utility_bills/view/StringLocalDateConverter.java
@@ -0,0 +1,17 @@
+package edu.project.utility_bills.view;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class StringLocalDateConverter extends StdConverter<String, LocalDate> {
+
+    @Override
+    public LocalDate convert(String value) {
+        if (value==null || value.trim().isEmpty()) {
+            return null;
+        }
+        return LocalDate.parse(value, DateTimeFormatter.ofPattern(LocalDateStringConverter.DATE_FORMAT));
+    }
+}

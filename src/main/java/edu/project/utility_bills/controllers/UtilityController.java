9f7162a6c83377529bfc2d899636c4c14162d1a0
assaulov <assaulov.i@gmail.com>
2021-02-09 18:15:02 +0200

    UtilityController turn on MVC controller

--- src/main/java/edu/project/utility_bills/controllers/UtilityController.java
+++ src/main/java/edu/project/utility_bills/controllers/UtilityController.java
@@ -9,19 +9,19 @@ import edu.project.utility_bills.view.UtilityResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("utility")
-public class UtilityListController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(UtilityListController.class);
+public class UtilityController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UtilityController.class);
 
 
     @Autowired
@@ -33,96 +33,100 @@ public class UtilityListController {
 
 
     @GetMapping("/{name}")
-    public ModelAndView findAll(@PathVariable String name) {
+    public String findAll(@PathVariable String name, Model model) {
         LOGGER.info("/ALL Get mapping, метод контроддера : showPage( )");
         ur.setUsername(name);
-        List<UtilityResponse> utilitiesList = utilityService.findAll(ur);
-        ModelAndView model = new ModelAndView();
-        model.setViewName("utility");
-        model.addObject("utilities", utilitiesList);
-        model.addObject("today", LocalDate.now().format(DateTimeFormatter.ISO_DATE));
-        return model;
+        List<UtilityResponse> responses = utilityService.findAll(ur);
+        model.addAttribute("utility");
+        model.addAttribute("utilities", responses);
+        model.addAttribute("today", LocalDate.now().format(DateTimeFormatter.ISO_DATE));
+        return "utility";
     }
 
 
     @GetMapping("{name}/findByDate")
-    public ModelAndView findUtilitiesByDate(ModelAndView model, @RequestParam("findByDate") String date, @PathVariable String name) {
+    public String findUtilitiesByDate(Model model, @RequestParam("findByDate") String date, @PathVariable String name) {
         LOGGER.info("/findByDate Get mapping, метод контроддера : findUtilitiesByDate( )");
-        model.setViewName("utility");
+        model.addAttribute("utility");
 
         try {
             ur.setDateOfWriteUtilityMeter(LocalDate.parse(date));
             List<UtilityResponse> responses = utilityService.findAllUtilitiesByDate(ur);
-            model.addObject("utilities", responses);
-            model.addObject("date", LocalDate.parse(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+            model.addAttribute("utilities", responses);
+            model.addAttribute("date", LocalDate.parse(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+            if(responses.isEmpty()){
+               findAll(name, model);
+            }
+
         } catch (DateTimeParseException e) {
             String str = "Введите дату!";
-            model.addObject("str", str);
+            model.addAttribute("str", str);
         }
-        return model;
+        return "utility";
     }
 
     @GetMapping("{name}/findByPeriod")
-    public  ModelAndView findUtilitiesByPeriod(ModelAndView model,
+    public  String findUtilitiesByPeriod(Model model,
                                                @RequestParam("dateFrom") String dateFrom,
                                                @RequestParam("dateTo") String dateTo, @PathVariable String name) {
         LOGGER.info("/findByPeriod Get mapping, метод контроддера : findUtilitiesByPeriod( )");
-        model.setViewName("utility");
+        model.addAttribute("utility");
 
         try {
             ur.setDateFrom(LocalDate.parse(dateFrom));
             ur.setDateTo(LocalDate.parse(dateTo));
              List<UtilityResponse> responses = utilityService.findUtilitiesByPeriod(ur);
-             model.addObject("utilities", responses);
-             model.addObject("dateFrom", LocalDate.parse(dateFrom).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-             model.addObject("dateTo", LocalDate.parse(dateTo).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+             model.addAttribute("utilities", responses);
+             model.addAttribute("dateFrom", LocalDate.parse(dateFrom).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+             model.addAttribute("dateTo", LocalDate.parse(dateTo).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         } catch (DateTimeParseException e) {
              String str = "Введите Период!";
-             model.addObject("str", str);
+             model.addAttribute("str", str);
         }
 
-        return model;
+        return "utility";
 
     }
 
     @PostMapping("{name}/delete")
-    public @ResponseBody ModelAndView deleteById(@RequestParam("utilityId") String utilityId, @PathVariable String name) {
+    public  String deleteById(@RequestParam("utilityId") String utilityId, @PathVariable String name) {
         LOGGER.info("/delete Post Mapping, метод контроддера : deleteById( )");
         ur.setUtilityId(Long.parseLong(utilityId));
         utilityService.deleteById(ur);
-    return findAll(name);
+
+
+        return "redirect:/utility/" + name;
     }
 
 
     @PostMapping("{name}/save")
-    public @ResponseBody ModelAndView saveUtility(Utilities utility, @PathVariable String name,  User user) {
+    public String saveUtility(Utilities utility, @PathVariable String name,  User user) {
         LOGGER.info("/save Post Mapping, метод контроддера : saveUtility( )");
-
-        System.out.println(name);
         user.setUsername(name);
         utility.setUser(user);
-              try{        LOGGER.info(" try  utilityService.save(utility)");
+              try{
+                  LOGGER.info(" try  utilityService.save(utility)");
 
-
+                  ur.setUsername(name);
                   utilityService.saveUtilities(utility);
-                  return findAll(name);
+
                 } catch (Exception e) {
 
                   LOGGER.info(e.toString());
-            return findAll(name);
+
         }
+              return "redirect:/utility/" + name;
     }
 
 
 
     @PutMapping(value = "{name}/update" )
-    public ModelAndView updateUtility(Utilities utility, @PathVariable String name) {
+    public String updateUtility(Utilities utility, @PathVariable String name) {
         LOGGER.info("/update Put Mapping, метод контроддера : updateUtility( )");
         LOGGER.info("Контроллер начинает работу");
 
-
-
-           ur.setUtilityId(utility.getUtilityId());
+        ur.setUsername(name);
+        ur.setUtilityId(utility.getUtilityId());
             LOGGER.info(String.valueOf(utility.getUtilityId())+ " ID счетчиков");
             LOGGER.info(ur.toString() + "Запрос контроллера");
             LOGGER.info("Запрос ушел в сервис ");
@@ -130,7 +134,7 @@ public class UtilityListController {
             LOGGER.info("Вернулся ответ");
 
 
-         return new ModelAndView(new RedirectView(""));
+         return "redirect:/utility/" + name;
 
     }
 
